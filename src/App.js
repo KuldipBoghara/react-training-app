22ec201161815585dfc8347389316ba5188d69ba
Mr-BK <bogharakuldip@gmail.com>
2022-04-21 13:23:15 -0400

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import UseMemoExample from './components/UseMemoExample';
 import UseRefExample3 from './components/UseRefExample3';
 
 function App() {
   return (
     <div className="container mt-5">
-      <UseRefExample3 />
+      <UseMemoExample />
     </div>
   );
 }
